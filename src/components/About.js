05d9c3d2f5bdd8c7f0aea4ff5e98b22c6896600e
Paulinendemo <pauline.ndemo@student.moringaschool.com>
2022-12-08 20:48:33 +0300

    done

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h1>What We Do</h1>
+      <p>
+      We are a real estate development firm <br></br>that seeks to develop institutional grade real <br></br>estate having a team with extensive and deep<br></br> experience in real estate development and finance.<br></br> We work to deliver quality and aspirational real estate<br></br> developments that encourage great community living.
+
+      </p>
+    </div>
+  );
+}
+
+export default About;
